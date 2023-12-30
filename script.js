4ef85dbf7384fe22643ea64509ccdd3210199d12
kajal202 <96862427+kajal202@users.noreply.github.com>
2023-12-30 17:05:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let turn = "X";
+let isgameover = false;
+let turnCount=0;
+// Function to change the turn
+const changeTurn = () => {
+  return turn === "X" ? "0" : "X";
+};
+
+// Function to check for a win
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
+  let wins = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+  ];
+  wins.forEach((e) => {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtext[e[0]].innerText + " Won";
+      isgameover = true;
+      
+    }
+    else if(turnCount==9){
+      document.querySelector(".info").innerText =
+         "It's a draw";
+         isgameover = true;
+    }
+  });
+};
+
+// // Game Logic
+// // music.play()
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turnCount++;
+      turn = changeTurn();
+      // audioTurn.play();
+      checkWin();
+      if (!isgameover) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
+    }
+  });
+});
+
+// Add onclick listener to reset button
+reset.addEventListener("click", () => {
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  turn = "X";
+  isgameover = false;
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+ 
+});
+
+
